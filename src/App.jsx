ee8325f252c66c88c9e0199402abfe65d81edbd4
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-06-30 14:47:11 +0100

    search bar functional added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-// import { useState, useEffect } from 'react'
-
+// import { useState, useEffect } from 'react';
+// import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+// import ImageCard from "./components/ImageCard/ImageCard";
+// import ImageGallery from "./components/ImageCard/ImageCard";
+// import ImageModal from "./components/ImageModal/ImageModal";
+// import Loader from "./components/Loader/Loader";
+// import LoadMoreBtn  from "./components/LoadMoreBtn/LoadMoreBtn";
+import SearchBar from "./components/SearchBar/SearchBar";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const handleSearch = (searchQuery) => {
+    console.log('Searching for:', searchQuery);
+  };
+  return (
+    <>
+      <SearchBar onSubmit={handleSearch} />
+    </>
+  );
 }
 
 export default App;
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,45 @@
-// import css from "./SearchBar.module.css";
+import css from "./SearchBar.module.css";
+import { useState } from "react";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+// import toast, { Toaster } from "react-hot-toast";
 
-const SearchBar = () => {
-  return <></>;
+const SearchBar = ({ onSubmit }) => {
+  const [query, setQuery] = useState("");
+
+  const notify = () => {};
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (query === "") {
+      notify();
+      return;
+    }
+    onSubmit(query.trim());
+    setQuery("");
+  };
+  return (
+    <>
+      <header className={css.searchWrapper}>
+        <form className={css.searchForm} onSubmit={handleSubmit}>
+          <div className={css.inputWrapper}>
+            <button className={css.searchBtn} type="submit">
+              <FaMagnifyingGlass className={css.searchIcon} />
+            </button>
+            <input
+              className={css.searchInput}
+              type="text"
+              name="search"
+              value={query}
+              autoComplete="off"
+              autoFocus
+              placeholder="Search images and photos"
+              onChange={(event) => setQuery(event.target.value)}
+            />
+          </div>
+        </form>
+      </header>
+    </>
+  );
 };
 
 export default SearchBar;
